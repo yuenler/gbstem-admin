1e882a79f337bb6ade161cd60cee6120be7d36ff
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-01-02 05:27:38 -0500

    format document

--- src/lib/components/forms/SetInterviewTimesForm.svelte
+++ src/lib/components/forms/SetInterviewTimesForm.svelte
@@ -38,7 +38,7 @@
   let interviewer = {
     interviewerFirstName: '',
     interviewerLastName: '',
-    interviewerEmail:'',
+    interviewerEmail: '',
   }
 
   let startId = allInterviewSlots.length + 1
@@ -50,15 +50,17 @@
       if ($user) {
         let include = true
         if ($user.object.displayName && $user.object.email) {
-          interviewer.interviewerFirstName = $user.object.displayName.split(' ')[0];
-          interviewer.interviewerLastName = $user.object.displayName.split(' ')[1];
-          interviewer.interviewerEmail = $user.object.email;
+          interviewer.interviewerFirstName =
+            $user.object.displayName.split(' ')[0]
+          interviewer.interviewerLastName =
+            $user.object.displayName.split(' ')[1]
+          interviewer.interviewerEmail = $user.object.email
           for (let intervalSlot of allInterviewSlots) {
             if (intervalSlot.id === doc.id) {
-              include = false;
+              include = false
             }
-            if(Number(intervalSlot.id) > startId) {
-              startId = Number(intervalSlot.id) + 1;
+            if (Number(intervalSlot.id) > startId) {
+              startId = Number(intervalSlot.id) + 1
             }
           }
           if (json['interviewSlotStatus'] === 'available' && include) {
@@ -78,10 +80,10 @@
         }
       }
     })
-    console.log(startId);
-    console.log($user?.object.email);
-    console.log(allInterviewSlots);
-    return allInterviewSlots;
+    console.log(startId)
+    console.log($user?.object.email)
+    console.log(allInterviewSlots)
+    return allInterviewSlots
   }
 
   async function clearData() {
@@ -99,27 +101,26 @@
   }
 
   function addTime(timeSlot: Data.TimeSlot) {
-    startId = startId + 1;
-        allInterviewSlots.push({
-          date: timeSlot.date.toString(),
-          id: startId.toString(),
-          interviewerFirstName: interviewer.interviewerFirstName,
-          interviewerLastName: interviewer.interviewerLastName,
-          intervieweeFirstName: '',
-          intervieweeLastName: '',
-          intervieweeId: '',
-          interviewerEmail: interviewer.interviewerEmail,
-          interviewLink: timeSlot.link,
-          interviewSlotStatus: 'available',
-        })
-        data = getData()
-      
-        console.log(allInterviewSlots)
-      allInterviewSlots.forEach((interview) =>
-      setDoc(doc(db, 'instructorInterviewTimes', interview.id), interview)
+    startId = startId + 1
+    allInterviewSlots.push({
+      date: timeSlot.date.toString(),
+      id: startId.toString(),
+      interviewerFirstName: interviewer.interviewerFirstName,
+      interviewerLastName: interviewer.interviewerLastName,
+      intervieweeFirstName: '',
+      intervieweeLastName: '',
+      intervieweeId: '',
+      interviewerEmail: interviewer.interviewerEmail,
+      interviewLink: timeSlot.link,
+      interviewSlotStatus: 'available',
+    })
+    data = getData()
+
+    console.log(allInterviewSlots)
+    allInterviewSlots.forEach((interview) =>
+      setDoc(doc(db, 'instructorInterviewTimes', interview.id), interview),
     )
-    }
-  
+  }
 
   function addRange(timeRange: Data.TimeRange) {
     startId = startId + 1
@@ -174,19 +175,18 @@
   }
 
   async function deleteTime(interview: Data.InterviewSlot) {
-    if(interview.interviewerEmail !== interviewer.interviewerEmail) {
-      alert.trigger('error', 'This interview does not belong to you!');
+    if (interview.interviewerEmail !== interviewer.interviewerEmail) {
+      alert.trigger('error', 'This interview does not belong to you!')
     } else {
       await deleteDoc(doc(db, 'instructorInterviewTimes', interview.id)).then(
-      () => {
-        data = clearData()
-        data = getData()
-        alert.trigger('success', 'Timeslot successfully deleted.')
-      },
-    )
+        () => {
+          data = clearData()
+          data = getData()
+          alert.trigger('success', 'Timeslot successfully deleted.')
+        },
+      )
     }
   }
-
 </script>
 
 {#await data then value}
@@ -198,7 +198,11 @@
       <Card>
         <h2 class="font-bold">Add A Time Slot</h2>
         <Input type="datetime-local" bind:value={time.date} label="Set Date" />
-        <Input type="text" bind:value={time.link} label="Interview Meeting Link" />
+        <Input
+          type="text"
+          bind:value={time.link}
+          label="Interview Meeting Link"
+        />
         <div class="right-2 items-center">
           <Button
             color="blue"
@@ -209,9 +213,13 @@
       </Card>
 
       <div class="flex gap-5 my-5">
-      <Input type="checkbox" bind:value={editSlots} label="Edit Slots"/>
-      <Input type="checkbox" bind:value={includeAllSlots} label="Include All Interview Time Slots"/>
-    </div>
+        <Input type="checkbox" bind:value={editSlots} label="Edit Slots" />
+        <Input
+          type="checkbox"
+          bind:value={includeAllSlots}
+          label="Include All Interview Time Slots"
+        />
+      </div>
       <!-- <Card>
         <h2 class="font-bold">Add Time Range</h2>
         <Input type="date" bind:value={timeRanges.date} label="Set Date" />
@@ -237,56 +245,56 @@
       </Card> -->
     </div>
 
-      {#each value as interview}
+    {#each value as interview}
       {#if editSlots}
-      {#if (!includeAllSlots && interview.interviewerEmail === interviewer.interviewerEmail) || includeAllSlots}
-      <Card>
-        <Form
-          class={clsx(showValidation && 'show-validation', className)}
-          on:submit={() => updateTime(interview)}
-        >
-          <div style="padding:1rem;">
-            <div><b>Interviewer: </b>{interview.interviewerFirstName}</div>
-            <Input
-              type="datetime-local"
-              bind:value={interview.date}
-              label="Edit Interview Meeting Time"
-            />
-            <Input
-              type="text"
-              bind:value={interview.interviewLink}
-              label="Edit Interview Meeting Link"
-            />
-            <div class="flex gap-5">
-              <div class="right-2 items-center">
-                <Button
-                  color="blue"
-                  class="px-2 py-1 my-4"
-                  on:click={() => updateTime(interview)}>Save</Button
-                >
-              </div>
-              <div class="right-2 items-center">
-                <Button
-                  color="blue"
-                  class="px-2 py-1 my-4"
-                  on:click={() => deleteTime(interview)}>Delete</Button
-                >
-              </div>
-            </div>
-          </div>
-        </Form>
-      </Card> 
-      {/if}
-        {:else}
         {#if (!includeAllSlots && interview.interviewerEmail === interviewer.interviewerEmail) || includeAllSlots}
+          <Card>
+            <Form
+              class={clsx(showValidation && 'show-validation', className)}
+              on:submit={() => updateTime(interview)}
+            >
+              <div style="padding:1rem;">
+                <div><b>Interviewer: </b>{interview.interviewerFirstName}</div>
+                <Input
+                  type="datetime-local"
+                  bind:value={interview.date}
+                  label="Edit Interview Meeting Time"
+                />
+                <Input
+                  type="text"
+                  bind:value={interview.interviewLink}
+                  label="Edit Interview Meeting Link"
+                />
+                <div class="flex gap-5">
+                  <div class="right-2 items-center">
+                    <Button
+                      color="blue"
+                      class="px-2 py-1 my-4"
+                      on:click={() => updateTime(interview)}>Save</Button
+                    >
+                  </div>
+                  <div class="right-2 items-center">
+                    <Button
+                      color="blue"
+                      class="px-2 py-1 my-4"
+                      on:click={() => deleteTime(interview)}>Delete</Button
+                    >
+                  </div>
+                </div>
+              </div>
+            </Form>
+          </Card>
+        {/if}
+      {:else if (!includeAllSlots && interview.interviewerEmail === interviewer.interviewerEmail) || includeAllSlots}
         <Card>
-                <div class="my-1"><b>Interviewer:</b> {interview.interviewerFirstName}</div>
-                <b>Time:</b> {new Date(interview.date).toLocaleString()}
+          <div class="my-1">
+            <b>Interviewer:</b>
+            {interview.interviewerFirstName}
+          </div>
+          <b>Time:</b>
+          {new Date(interview.date).toLocaleString()}
         </Card>
-        {/if}
-
-       
-        {/if}
-      {/each}   
-    </Form>
+      {/if}
+    {/each}
+  </Form>
 {/await}
