9311a25b25304c41f4c1e5f238ef2cc46dd06be3
Yuen Ler Chow <lerchow@gmail.com>
2024-01-20 17:44:43 -0500

    Add interview icon to application page

--- src/routes/(signedIn)/(emailVerified)/applications/+page.svelte
+++ src/routes/(signedIn)/(emailVerified)/applications/+page.svelte
@@ -402,6 +402,19 @@
                   clip-rule="evenodd"
                 />
               </svg>
+            {:else if application.values.meta.decision.type === 'interview'}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-5 h-5 text-blue-300"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             {/if}
           {:else}
             None
