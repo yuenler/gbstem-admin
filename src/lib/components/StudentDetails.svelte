e4337f26ee643b799fe0abe278cc426d85b232b7
Kendree <94144671+kchenturtles@users.noreply.github.com>
2025-01-07 04:50:47 -0500

    improve page load

--- src/lib/components/StudentDetails.svelte
+++ src/lib/components/StudentDetails.svelte
@@ -111,7 +111,7 @@
 </script>
 
 <Dialog bind:this={dialogEl} size="full" alert>
-    <svelte:fragment slot="title"><div class="flex" style="justify-content:space-between;"><div style="align-content:center;">Student Attendance and Information</div><div><Button color = 'red' on:click={() => {loading = false; dialogEl.cancel()}}>Close</Button></div></div></svelte:fragment>
+    <svelte:fragment slot="title"><div class="flex" style="justify-content:space-between;"><div style="align-content:center;">Student Attendance and Information</div><div><Button color = 'red' on:click={() => {loading = true; id = undefined; dialogEl.cancel()}}>Close</Button></div></div></svelte:fragment>
     <div slot="description">
     <div class="mt-4 justify-center">
     {#each classes as value, i}
