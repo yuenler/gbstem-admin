2e948478ea89941ae07fbdc5c93387f4854a16d1
Kendree <94144671+kchenturtles@users.noreply.github.com>
2026-03-28 18:33:42 -0400

    courses update

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -13,10 +13,19 @@ import engineeringCoursesJson from './engineeringCourses.json'
 import gradesJson from './grades.json'
 import parentEducationJson from './parentEducation.json'
 import timeSlotsJson from './timeSlots.json'
-import coursesJson from './courses.json'
+import fallCourses from './fallCourses.json'
+import springCourses from './springCourses.json'
 import classesPerWeekJson from './classesPerWeek.json'
 import daysOfWeekJson from './daysOfWeek.json'
 import interviewAttendanceJson from './interviewAttendance.json'
+
+const coursesJson = []
+
+if (new Date().getMonth() >= 7) {
+  coursesJson.push(...fallCourses)
+} else {
+  coursesJson.push(...springCourses)
+}
 
 export {
   raceJson,
